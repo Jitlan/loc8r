6879776218f06fbeaf911752efefe5a1957c07c4
Jordan Liebling <jliebling1994@gmail.com>
2017-10-09 21:24:01 -0400

    fixed some of the files and their placement

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var ctrlMain = require('../controllers/main');
+
+/*var homepageController = function (req, res){ <---Where router looks for URL
+  res.render('index', {title: 'Express'}) <---Controller content
+};*/
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', ctrlMain.index);
 
 module.exports = router;
+
